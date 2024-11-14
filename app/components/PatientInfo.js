53855d0b5110f9e4d526181e5b67a4c2eff35077
hafidz-hornet <hafidz@hornet-technology.com>
2024-11-14 11:53:58 +0800

    Added PatientInfo component

--- app/components/PatientInfo.js
+++ app/components/PatientInfo.js
@@ -1,14 +1,1 @@
-import React from 'react';
-
-function PatientInfo({ patient }) {
-  return (
-    <div>
-      <h2>Patient Information</h2>
-      <p>Name: {patient.name}</p>
-      <p>Age: {patient.age}</p>
-      {/* Add other patient details as needed */}
-    </div>
-  );
-}
-
-export default PatientInfo;
+Created PatientInfo component to manage patient details in the app
