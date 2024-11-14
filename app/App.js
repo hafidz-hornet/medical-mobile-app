6e143414a26c46e2db1ab316770a8a7e7e7d5015
hafidz-hornet <hafidz@hornet-technology.com>
2024-11-14 11:53:56 +0800

    Initial commit - App.js file added

--- app/App.js
+++ app/App.js
@@ -1,13 +1,1 @@
-import React from 'react';
-import './index.css';
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Medical Mobile App</h1>
-      <p>This is a boilerplate React app.</p>
-    </div>
-  );
-}
-
-export default App;
+Initial commit with App.js file to start the application
