6cac4516747fee227321db590a8703609161a923
hafidz-hornet <hafidz@hornet-technology.com>
2024-11-14 11:53:57 +0800

    Added Appointment component

--- app/components/Appointment.js
+++ app/components/Appointment.js
@@ -1,14 +1,1 @@
-import React from 'react';
-
-function Appointment({ appointment }) {
-  return (
-    <div>
-      <h2>Appointment Details</h2>
-      <p>Date: {appointment.date}</p>
-      <p>Time: {appointment.time}</p>
-      {/* Add other appointment details as needed */}
-    </div>
-  );
-}
-
-export default Appointment;
+Created Appointment component for managing appointments in the app
